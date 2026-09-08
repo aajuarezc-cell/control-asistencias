4fe2f7b26aba01875c084a28d240fe2ba54a9a0f
aajuarezc-cell <aajuarezc@gmail.com.com>
2026-09-08 08:58:04 -0600

    vaciado de 30 dias

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ==========================================
 const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://tu_usuario:tu_password@cluster.mongodb.net/control_asistencias?retryWrites=true&w=majority';
 
-mongoose.connect(MONGO_URI);
-
+mongoose.connect(MONGO_URI)
 .then(() => console.log("🟢 Conectado exitosamente a MongoDB"))
 .catch(err => console.error("🔴 Error conectando a MongoDB:", err));
 
